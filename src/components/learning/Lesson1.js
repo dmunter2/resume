b4c5aba694c758bed746a9b7f89a7d590b48a122
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-01-19 06:45:03 -0800

    early on -styling and events added

--- src/components/learning/Lesson1.js
+++ src/components/learning/Lesson1.js
@@ -1,6 +1,18 @@
 import React, {useState} from 'react';
 import Style from 'styled-components';
 
+
+const DIV = Style.div`
+background-color: #1F2020;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+padding: 2%; 
+height: 100%;
+`
+
+
+
 const SHAPE = Style.div`
 height: 200px;
 width: 250px;
@@ -13,12 +25,43 @@ width: 50px;
 border: 1px solid red;
 margin: 1%;
 `
-const CONTAINER = Style.div``
+const CONTAINER = Style.div`
+display: flex;
+width: 250px;
+
+flex-direction: row;
+justify-content: space-between;
+`
+const DISPLAY = Style.div``
+
 
 const BUTTON = Style.button`
 width: 70px;
 height: 40px;
 `
+
+
+const H1 = Style.h1`
+color: #E4E4E2;
+`
+
+const H4 = Style.h4`
+color: #E4E4E2;
+`
+
+const DIVIDE = Style.div`
+width: 50%;
+`
+
+const DIVIDE2 = Style.div`
+width: 50%;
+display: flex;
+flex-direction: column;
+align-items: end;
+`
+
+
+
 
 
 const Lesson1 = () => {
@@ -28,6 +71,10 @@ const Lesson1 = () => {
     const [button, setButton] = useState('plain-btn')
 
     const [button2, setButton2] = useState('button-design')
+
+    const [addDisplay, setaddDisplay] = useState('no-display')
+
+    const [toggleButton, setToggleButton]= useState('plain-btn')
 
 
     const handleDisplay = e => {
@@ -43,26 +90,53 @@ const Lesson1 = () => {
         setButton('plain-btn')
     }
 
+    const handleDisplay3 = e => {
+        e.preventDefault();
+        if(addDisplay = 'no-display'){
+            return setaddDisplay('add-display')
+        } else {
+            return setaddDisplay('no-display')
+        }
+        
+
+    }
 
     return (
-        <div>
-            <h1>My First Lesson</h1>
-            <h4>One of the first things I learned at Lambda School was how to use 
-                flexbox. I loved the feeling of creating something and then having control
-                on that contents display through out the page regardless of the screen size
-            </h4>
-            <CONTAINER>
-                <BUTTON onClick={handleDisplay} className={button} >Row</BUTTON>
-                <BUTTON onClick={handleDisplay2} className={button2}>Column</BUTTON>
-            </CONTAINER>
+        <DIV>
 
-            <SHAPE className={display}>
-                <DIV1>Shape 1</DIV1>
-                <DIV1>Shape 2</DIV1>
-                <DIV1>Shape 3</DIV1>
+            <DIVIDE>
+                <H1>Early On</H1>
+                <H4>One of the first things I learned at Lambda School was how to use
+                    flexbox. I loved the feeling of creating something and then having control
+                    on that contents display through out the page regardless of the screen size
+                </H4>
+            </DIVIDE>
 
-            </SHAPE>
-        </div>
+
+            <DIVIDE2>
+                <CONTAINER>
+                    <DISPLAY>
+                        <BUTTON onClick={handleDisplay3} className={toggleButton}>Display</BUTTON>
+                    </DISPLAY>
+
+                    <DISPLAY>
+                        <BUTTON onClick={handleDisplay} className={button} >Row</BUTTON>
+                        <BUTTON onClick={handleDisplay2} className={button2}>Column</BUTTON>
+                    </DISPLAY>
+
+             
+                </CONTAINER>
+
+                <SHAPE className={display} className={addDisplay}>
+                    <DIV1>Shape 1</DIV1>
+                    <DIV1>Shape 2</DIV1>
+                    <DIV1>Shape 3</DIV1>
+
+                </SHAPE>
+            </DIVIDE2>
+     
+      
+        </DIV>
     )
 }
 
--- src/components/learning/Lesson2.js
+++ src/components/learning/Lesson2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Style from 'styled-components';
 
 const H1 = Style.h1``
-
+const DIV = Style.div``
 
 const Lesson2 = () => {
 
@@ -10,6 +10,16 @@ const Lesson2 = () => {
         <div>
 
             <H1>Digging into Javascript</H1>
+            <h5>After creating many website templates with HTML and CSS I was ready for 
+                the life and blood of many websites; Javascript.
+            </h5>
+            <h5>After we learned the basics we covered how to implement them in functions </h5>
+            <h5>Functions are the foundation for all web interactions</h5>
+            <h5>It was here where i was able to complete projects like this</h5>
+
+            <DIV>
+                <h1>This is where my js code will go</h1>
+            </DIV>
         </div>
 
     )

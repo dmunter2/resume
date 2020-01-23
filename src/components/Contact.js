9eae4b41bffc10a912ab211968730cb30a9a6ef0
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-01-22 19:43:19 -0800

    responsive on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,6 +11,15 @@ height: 200px;
 border-radius: 5px;
 margin: 15px;
 
+@media (max-width: 600px) {
+  width: 85px;
+  height: 70px;
+
+  display: flex;
+  align-items: center;
+}
+
+
 `
 const CONTAINER = Style.div`
 width: 220px;
@@ -23,6 +32,15 @@ justify-content: center;
 background-color: #f9f6ef;
 box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
 
+@media (max-width: 600px) {
+  width: 250px;
+  height: 70px;
+
+  display: flex;
+  align-items: center;
+}
+
+
 
 `
 const DISPLAY = Style.div`
@@ -31,9 +49,26 @@ flex-direction: row;
 justify-content: center;
 flex-wrap: wrap;
 margin-top: 20px;
+
+@media (max-width: 600px) {
+
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
 `
 const H1 = Style.h5`
 text-align: center;
+
+@media (max-width: 600px) {
+
+
+  margin: 1%;
+}
+
 
 `
 const DIV = Style.div`
@@ -41,17 +76,45 @@ margin-top: 100px;
 display: flex;
 flex-direction: row;
 justify-content: center;
+
+@media (max-width: 600px) {
+  display: none;
+}
+
+
 `
 
 const IMG2 = Style.img`
 height: 150px;
+
+@media (max-width: 600px) {
+  width: 85px;
+  height: 70px;
+
+  display: flex;
+  align-items: center;
+}
+
+
 `
 const MAIN = Style.div`
 display: flex;
 flex-direction: column;
 justify-content: center;
 `
+const NEW = Style.div`
+display: flex;
+flex-direction: row;
+`
+const DIV4 = Style.div`
+display: none;
 
+@media (max-width: 600px) {
+  display: flex;
+  margin-top: 5%;
+}
+
+`
 
 
 const Contact = () => {
@@ -61,12 +124,26 @@ const Contact = () => {
         <MAIN>
 
             <DISPLAY>
-                <a href='https://www.linkedin.com/in/devon-munter-93890a182'>
-                    <IMG src={link} />
-                </a>
-                <a href='https://www.facebook.com/devon.munter'>
-                    <IMG src={face} />
-                </a>
+                <NEW>
+
+                    <a href='https://www.linkedin.com/in/devon-munter-93890a182'>
+                        <IMG src={link} />
+                    </a>
+                    <a href='https://www.facebook.com/devon.munter'>
+                        <IMG src={face} />
+                    </a>
+
+                    <DIV4>
+                        <a href='https://github.com/dmunter2'>
+                            <IMG2 src={git} />
+
+                        </a>
+
+                    </DIV4>
+
+
+                </NEW>
+
                 <CONTAINER>
 
                     <H1>devon.munter@gmail.com</H1>
